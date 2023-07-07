2a29715e7c155f6286018863f91957f87f31078e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-07-07 06:45:29 +0100

    Added more testing packages

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const startupDebugger = require("debug")("app:startup");
 const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+/*Routes*/
+
+if (app.get("env") === "development") {
+  app.use(morgan("dev"));
+  startupDebugger("hello world");
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
