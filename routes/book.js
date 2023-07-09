c2f1b40f936342908b2e613f909d45c5a8d92bec
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-07-09 13:53:13 +0100

    Updated book route

--- routes/book.js
+++ routes/book.js
@@ -4,6 +4,34 @@ const Router = express.Router();
 Router.use((req, res, next) => {
   next();
 });
+
+const books = [
+  {
+    id: 1,
+    name: "HTML5 for beginners",
+  },
+
+  {
+    id: 2,
+    name: "Javascript for beginners",
+  },
+
+  {
+    id: 3,
+    name: "PHP for beginners",
+  },
+
+  {
+    id: 4,
+    name: "C++ for dummies",
+  },
+
+  {
+    id: 5,
+    name: "Java for babies",
+  },
+];
+
 Router.get("/", (req, res) => {
   res.json(200, "Hello from books endpoint");
 });
