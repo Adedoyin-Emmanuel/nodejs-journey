9aa17410bc2ac47c68cd446b087a167586655f0b
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-07-23 19:50:33 +0100

    Updated bookSchema

---
+++ models/book.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const bookSchema = mongoose.Schema({
+  id: { type: "string", unique: true },
+  name: { type: "string" },
+  author: { type: "string" },
+  createdAt: { type: "datetime", default: new Date.now() },
+});
+
+const bookModel = mongoose.model("Book", bookSchema);
+
+module.exports = {
+  bookSchema,
+};
