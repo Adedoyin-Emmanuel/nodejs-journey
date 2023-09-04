92bed8a54cc106f6351695833b262132253b5f0d
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-04 08:06:21 +0100

    Connected to mongodb successfully

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 require("dotenv").config();
 const debug = require("debug")("app:startup");
+const dbDebug = require("debug")("app:db");
 const PORT = process.env.PORT || 2800;
 const helmet = require("helmet");
+const morgan = require("morgan");
 const { homeRoute, bookRoute } = require("./routes/index");
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 app.use(helmet());
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.use("/api/books", bookRoute);
 app.use("/", homeRoute);
+app.use("/api/books", bookRoute);
 
 app.listen(PORT, () => {
   debug(`App Listening On PORT ${PORT}`);
+  mongoose
+    .connect(process.env.LOCAL_MONGODB_URL)
+    .then(() => {
+      dbDebug("Connected to Mongodb");
+    })
+    .catch((error) => dbDebug(`Couldn't connect to Mongodb ${error}`));
 });
--- routes/home.route.js
+++ routes/home.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 
-router.get("/", (res, req, next) => {
+router.get("/", (req, res, next) => {
   res.status(200).send({ message: "Welcome to the books API!" });
   next();
 });
